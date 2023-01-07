1f9b946ee02a28ee958513b46e5b8fdbbc9273d2
humphrey777 <song.heping01@gmail.com>
2023-01-07 02:03:30 -0500

    adding Main.java, updated from 'num >= 0 and num < 6' to 'num>=6' since 'if it is equal or greater 6'

---
+++ src/main/java/ca/vanier/Main.java
@@ -0,0 +1,10 @@
+package ca.vanier;
+
+public class Main {
+    public static void main (String[] args) {
+        //Execute
+        String message = ScoreManager.execute(args);
+
+        System.out.println(message);
+    }
+}
