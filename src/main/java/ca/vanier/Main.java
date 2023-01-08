675ba868aff55efc14c5b4a3aec6823bb7cff3b5
yateng198 <yatenggeng@gmail.com>
2023-01-08 14:39:34 -0500

    Last modification

--- src/main/java/ca/vanier/Main.java
+++ src/main/java/ca/vanier/Main.java
@@ -3,7 +3,8 @@ package ca.vanier;
 public class Main {
     
     public static void main(String[] args) throws Exception {
-
+        //Invoke the execute method and pass args string array as a parameter
+        //and print out the result returned.
         System.out.println(ScoreManager.execute(args));
 
     }
--- src/main/java/ca/vanier/ScoreManager.java
+++ src/main/java/ca/vanier/ScoreManager.java
@@ -3,14 +3,14 @@ package ca.vanier;
 public class ScoreManager {
 
     public static String execute(String[] args) {
-        //identify a variable to recieve a input number
+        //Identify a variable to recieve a input number
         int num = Integer.parseInt(args[0]);
 
-        //condition
-        if (num >= 6) {
-            return "true";
+        //Condition, returns true / false if the score received is equal or greater than 6
+        if (num >= 0 && num < 6) {
+            return "True";
         }
-        return "false";
+        return "False";
     }
 
 }
