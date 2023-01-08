1c9e0f45b52fd61ae84a810644c46bfc5ce0469d
yateng198 <yatenggeng@gmail.com>
2023-01-08 14:46:12 -0500

    Test file correction

--- src/test/java/ca/vanier/TestScore.java
+++ src/test/java/ca/vanier/TestScore.java
@@ -11,7 +11,7 @@ public class TestScore {
         String result = ScoreManager.execute(args);
         //returns true / false if args[0] is equal or greater than 6
         //We are hard coding args[0] to be 4 which means result should be true as we expected in the Assert
-        Assert.assertEquals("true", result);
+        Assert.assertEquals("True", result);
         //Test pass will print this message
         System.out.println("Test pass!");
 
